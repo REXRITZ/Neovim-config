d6559f287ffadc4cc89fc9f28e4c35bfce15fdc3
REXRITZ <rmbhadane33@gmail.com>
2024-10-09 21:49:49 +0530

    catppuccin added

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,18 +1,30 @@
 return { 
-	"rose-pine/neovim",
-	lazy = false,
-	priority = 1000,
-	name = "rose-pine",
-	config = function()
-        require("rose-pine").setup({
-            styles = {
-                bold = true,
-                italic = true,
-                transparency = false,
-            },
-        })
-		vim.cmd("colorscheme rose-pine")
-	end
+    {
+        "rose-pine/neovim",
+	    lazy = false,
+	    priority = 1000,
+    	name = "rose-pine",
+	    config = function()
+            require("rose-pine").setup({
+                styles = {
+                    bold = true,
+                    italic = true,
+                    transparency = false,
+                },
+            })
+	    	-- vim.cmd("colorscheme rose-pine")
+    	end
+    },
+    { 
+        "catppuccin/nvim", 
+        name = "catppuccin", 
+        priority = 1000,
+        config = function()
+            require("catppuccin").setup({
+            })
+            vim.cmd("colorscheme catppuccin")
+        end
+    }
 	
 
 }
